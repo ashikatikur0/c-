5cb1d0f251550d306f9cd2b8bca2ec465d9a6b85
Ashik_X <ashikatikur01@gmail.com>
2026-01-04 11:06:15 +0600

    Add files via upload

---
+++ const.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+int main(){
+    const double PI = 3.14159;
+    const int radius = 10;
+    double area = 2 *PI * radius * radius;
+    std::cout << "Area of circle: " << area << std::endl;
+}
---
+++ helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+int main() {
+    // comment 
+    // int x= 42;
+    // int y= 45;
+    char name = 'A';
+
+    // std::cout << "Character: " << name << std::endl;
+    std :: string greeting = "Hello, World!";
+    std :: cout << greeting << std :: endl;
+    
+    
+    return 0;
+}
